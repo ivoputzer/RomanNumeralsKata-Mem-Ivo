0596f32693670c99fcfaab272835d064cfb4538a
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2016-03-21 16:18:31 +0100

    adds smart edge conversion cases; refactoring

--- src/main/RomanNumerals.java
+++ src/main/RomanNumerals.java
@@ -1,33 +1,43 @@
 package main;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class RomanNumerals {
+
+	private static final Map<Integer, String> ROMAN_NUMBERS = new LinkedHashMap<Integer, String>();
 	
-	private static Map<Integer, String> values = new LinkedHashMap<Integer, String>();
 	static {
-		values.put(1000,"M");
-		values.put(500, "D");
-		values.put(100, "C");
-		values.put(50,  "L");
-		values.put(10,  "X");
-		values.put(5,   "V");
-		values.put(1,   "I");
+		ROMAN_NUMBERS.put(1000, "M");
+		ROMAN_NUMBERS.put(900, "CM");
+		ROMAN_NUMBERS.put(500, "D");
+		ROMAN_NUMBERS.put(400, "CD");
+		ROMAN_NUMBERS.put(100, "C");
+		ROMAN_NUMBERS.put(90, "XC");
+		ROMAN_NUMBERS.put(50, "L");
+		ROMAN_NUMBERS.put(40, "XL");
+		ROMAN_NUMBERS.put(10, "X");
+		ROMAN_NUMBERS.put(9, "IX");
+		ROMAN_NUMBERS.put(5, "V");
+		ROMAN_NUMBERS.put(4, "IV");
+		ROMAN_NUMBERS.put(1, "I");
 	}
- 	
-	public static String fromDecimal(Integer decimal) {	
-		if(decimal == 0)
+
+	public static String fromDecimal(Integer decimal) {
+		if (decimal == 0)
 			return "";
-		int step = greatestRomanNumberEquivalentFor(decimal);
-		return values.get(step) + fromDecimal(decimal - step);
+		int greatestRomanNumberEquivalent = greatestRomanNumberEquivalentFor(decimal);
+		return ROMAN_NUMBERS.get(greatestRomanNumberEquivalent) + fromDecimal(decimal - greatestRomanNumberEquivalent);
 	}
-	
-	public static int greatestRomanNumberEquivalentFor(Integer decimal){
-		for (Map.Entry<Integer, String> item: values.entrySet()) {
+
+	public static int greatestRomanNumberEquivalentFor(Integer decimal) {
+		for (Map.Entry<Integer, String> item : ROMAN_NUMBERS.entrySet()) {
 			if (decimal >= item.getKey())
 				return item.getKey();
 		}
 		return 0;
 	}
+	
 }
--- src/test/RomanNumeralsTest.java
+++ src/test/RomanNumeralsTest.java
@@ -23,25 +23,39 @@ public class RomanNumeralsTest {
 		assertEquals("D", RomanNumerals.fromDecimal(500));
 		assertEquals("M", RomanNumerals.fromDecimal(1000));
 	}
-		
+
 	@Test
 	public void testSmallestPossibileDecimalStep() {
 		assertEquals(0, RomanNumerals.greatestRomanNumberEquivalentFor(0));
 		assertEquals(1, RomanNumerals.greatestRomanNumberEquivalentFor(1));
 		assertEquals(1, RomanNumerals.greatestRomanNumberEquivalentFor(3));
 		assertEquals(5, RomanNumerals.greatestRomanNumberEquivalentFor(7));
-		
+
 	}
 
 	@Test
 	public void testSucceedingRomanNumerals() {
-		 assertEquals("VI", RomanNumerals.fromDecimal(6));
-		 assertEquals("VIII", RomanNumerals.fromDecimal(8));
+		assertEquals("VI", RomanNumerals.fromDecimal(6));
+		assertEquals("VIII", RomanNumerals.fromDecimal(8));
 	}
-	
-	
+
+	@Test
 	public void testPrecedingRomanNumerals() {
-		 assertEquals("IV", RomanNumerals.fromDecimal(4));
-		 assertEquals("IX", RomanNumerals.fromDecimal(10));
+		assertEquals("IV", RomanNumerals.fromDecimal(4));
+		assertEquals("IX", RomanNumerals.fromDecimal(9));
+		assertEquals("XL", RomanNumerals.fromDecimal(40));
+		assertEquals("XC", RomanNumerals.fromDecimal(90));
+		assertEquals("CD", RomanNumerals.fromDecimal(400));
+		assertEquals("CM", RomanNumerals.fromDecimal(900));
 	}
+
+	@Test
+	public void testComplexNumbers() {
+		assertEquals("MCMXCIX", RomanNumerals.fromDecimal(1999));
+		assertEquals("DCXCI", RomanNumerals.fromDecimal(691));
+		assertEquals("XXVII", RomanNumerals.fromDecimal(27));
+		assertEquals("MMDCCXXXVII", RomanNumerals.fromDecimal(2737));
+
+	}
+
 }
